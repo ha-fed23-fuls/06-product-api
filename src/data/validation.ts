29a011f4e3585a585dbcdb9f2972cf81afcfa8da
David Andersson <lejonmanen@gmail.com>
2024-09-04 15:18:16 +0200

    PUT /products/:id med validering

--- src/data/validation.ts
+++ src/data/validation.ts
@@ -1,21 +1,20 @@
 import Joi from 'joi'
 import { ToolNoId } from "./tools.js";
 
-const toolNoIdSchema = Joi.defaults(schema => {
-	return schema.required()
-})
-	.object({
+const toolNoIdSchema = Joi.object({
 		name: Joi.string()
 			.min(1),
 			// .required(),
 		price: Joi.number()
-			.min(0),
+			.min(0)
+			.strict(),  // inte okej med strängar som innehåller nummer typ "42"
 			// .required()
 		category: Joi.string()
 			.min(1)
 	}
-)
+).required()
 
+// Alternativt namn: isValidToolExceptId
 export function isToolNoId(data: ToolNoId): boolean {
 	let result = toolNoIdSchema.validate(data)
 	return !result.error
--- src/products.ts
+++ src/products.ts
@@ -3,8 +3,15 @@ import { tools, Tool, ToolNoId, addTool } from './data/tools.js'
 import { isToolNoId } from './data/validation.js'
 
 export const router: Router = express.Router()
-
 // Gå direkt till endpoints (konfigurationen görs i server.ts)
+
+
+// Interface som används i filen
+interface IdParam {
+	id: string;  // Alla URL-parametrar är strängar!
+}
+
+
 
 // OBS! '/products' står i server.ts  (''/products' + '/')
 router.get('/', (req: Request, res: Response) => {
@@ -12,13 +19,10 @@ router.get('/', (req: Request, res: Response) => {
 })
 
 // Svara med utvalt objekt
-interface IdParam {
-	id: string;  // Alla URL-parametrar är strängar!
-}
 router.get('/:id', (req: Request<IdParam>, res: Response) => {
 	// plocka ut ID från URL-parametrarna
 	const id: number = Number( req.params.id )
-	if( isNaN(id) || id < 0 ) {
+	if( !isValidId(id) ) {
 		// BAD REQUEST of id-parametern är en sträng eller tal mindre än noll
 		res.sendStatus(400)
 		return   // Viktigt att avsluta - annars kör vi SEND en gång till
@@ -34,7 +38,6 @@ router.get('/:id', (req: Request<IdParam>, res: Response) => {
 	}
 })
 
-
 // POST /products  <- lägga till en ny produkt, ha med BODY
 router.post('/', (req: Request<void, void, ToolNoId>, res: Response) => {
 	const newTool: ToolNoId = req.body
@@ -48,5 +51,45 @@ router.post('/', (req: Request<void, void, ToolNoId>, res: Response) => {
 	res.sendStatus(201)
 })
 
-// DELETE /products/:id  <- ta bort produkt baserat på id-parametern
 // PUT /products/:id  <- ha med BODY
+router.put('/:id', (req: Request<IdParam, void, ToolNoId>, res: Response) => {
+	// plocka ut id ur URL-param-objektet
+	// validera body, måste vara ett korrekt ToolNoId-objekt
+	// hitta index i listan för objektet som ska ändras
+	// byt ut objektet på indexet i listan mot det nya objektet
+	// skicka tillbaka statuskod
+
+	const id: number = Number(req.params.id)
+	if( !isValidId(id) ) {
+		res.sendStatus(400)  // Felaktigt id == 400 Bad request
+		return
+	}
+
+	const newTool: ToolNoId = req.body
+	console.log('typeof price: ' + (typeof newTool.price))
+	if( !isToolNoId(newTool) ) {
+		res.sendStatus(400)  // Felaktigt objekt == 400 Bad request
+		return
+	}
+
+	const index: number = tools.findIndex(tool => tool.id === id)
+	if( index === -1 ) {
+		res.sendStatus(404)  // Hittade inget objekt med samma id == 404 Not found
+		return
+	}
+
+	tools[index] = { ...newTool, id: id }
+	res.sendStatus(204)
+})
+
+
+// TODO
+// DELETE /products/:id  <- ta bort produkt baserat på id-parametern
+
+
+
+
+// Hjälpfunktioner
+function isValidId(maybeId: number): boolean {
+	return !isNaN(maybeId) && maybeId >= 0
+}
